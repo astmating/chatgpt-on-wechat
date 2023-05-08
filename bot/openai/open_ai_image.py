76dd7ee79e8eb96cfe76ea18b2e8041a0ff6c192
astmating <131445482+astmating@users.noreply.github.com>
2023-05-08 13:39:38 +0800

    open_ai_image.py

--- bot/openai/open_ai_image.py
+++ bot/openai/open_ai_image.py
@@ -27,7 +27,7 @@ class OpenAIImage(object):
             )
             image_url = response["data"][0]["url"]
             logger.info("[OPEN_AI] image_url={}".format(image_url))
-            return True, image_url
+            return True, image_url,"图片发回来了"
         except openai.error.RateLimitError as e:
             logger.warn(e)
             if retry_count < 1:
